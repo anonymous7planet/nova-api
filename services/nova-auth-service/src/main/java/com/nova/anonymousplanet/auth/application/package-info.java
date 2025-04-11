01346742dba44e761a26f0793d4c141c84bc55c6
anonymous7planet <anonymous7planet@gmail.com>
2025-04-11 12:15:48 +0900

    [Add] nova-auth-service 추가

---
+++ services/nova-auth-service/src/main/java/com/nova/anonymousplanet/auth/application/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 💡 애플리케이션 계층
+ * <p>
+ * 도메인 모델을 조합하여 유스케이스를 구현하는 서비스가 위치합니다.
+ */
+package com.nova.anonymousplanet.auth.application;
---
+++ services/nova-auth-service/src/main/java/com/nova/anonymousplanet/auth/interfaces/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 🌐 인터페이스 계층
+ * <p>
+ * 외부(HTTP 등)와의 통신을 담당하는 컨트롤러 및 DTO를 포함합니다.
+ */
+package com.nova.anonymousplanet.auth.interfaces;
